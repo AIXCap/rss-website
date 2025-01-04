f2aa1af44ea1c4a74b763ce7e1f615b3a258c895
isboyjc <214930661@qq.com>
2025-01-04 18:07:30 +0800

    refactor: 🔧 clean up page component by removing commented code and unused imports

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,17 +3,14 @@ export const runtime = 'edge'
 
 import Hero from './components/hero'
 import Features from './components/features'
-import Pricing from './components/pricing'
 import Evaluate from './components/evaluate'
 import GetFree from './components/get-free'
 import HotFeeds from './components/hot-feeds'
+// import Pricing from './components/pricing'
 export default function Home() {
   return (
     <div className="w-full">
       <Hero />
-      {/* <div className="z-10 flex min-h-64 items-center justify-center">
-        <TextReveal text="Magic UI will change the way you design." />
-      </div> */}
       <Features />
       <HotFeeds />
       {/* <Pricing /> */}
